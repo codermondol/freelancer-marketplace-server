9144f0ee7a513f55146de9a6dd4eb0044930c4cc
my first github repo <mrarifmondol98@gmail.com>
2026-04-06 11:51:43 -0800

    added new function for task management

--- index.js
+++ index.js
@@ -29,31 +29,31 @@ async function run() {
 
     const db = client.db("marketplace_db");
     const jobCollection = db.collection("allJobs");
+    const taskCollection = db.collection("tasks");
 
-    // add jobs 
+    // add jobs
 
-    app.get('/allJobs', async(req, res) => {
-
+    app.get("/allJobs", async (req, res) => {
       // const jobFields = {title: 1, coverImage: 1, postedBy: 1, category: 1}
       // const cursor = jobCollection.find().sort({postedAt: 1}).limit(6).project(jobFields);
 
       const email = req.query.userEmail;
       const query = {};
-      if(email) {
+      if (email) {
         query.userEmail = email;
       }
 
-      const cursor = jobCollection.find(query)
-      const result = await cursor.toArray()
-      res.send(result)
-    })
-
-    app.get('/allJobs/:id', async(req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id)};
-      const result = await jobCollection.findOne(query)
+      const cursor = jobCollection.find(query);
+      const result = await cursor.toArray();
       res.send(result);
-    })
+    });
+
+    app.get("/allJobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobCollection.findOne(query);
+      res.send(result);
+    });
 
     app.post("/allJobs", async (req, res) => {
       const newJob = req.body;
@@ -61,33 +61,53 @@ async function run() {
       res.send(result);
     });
 
-    app.patch('/allJobs/:id', async(req, res) => {
+    app.patch("/allJobs/:id", async (req, res) => {
       const id = req.params.id;
       const updatedAllJobs = req.body;
-      const query = { _id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const update = {
         $set: {
           title: updatedAllJobs.title,
-          postedBy: updatedAllJobs.postedBy
-        }
-      }      
-      const result = await jobCollection.updateOne(query, update)
+          postedBy: updatedAllJobs.postedBy,
+        },
+      };
+      const result = await jobCollection.updateOne(query, update);
+      res.send(result);
+    });
+
+    app.delete("/allJobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // Task manage APis
+
+    app.get("/tasks", async (req, res) => {
+      const email = req.query.userEmail;
+      const query = {};
+      if (email) {
+        query.userEmail = email;
+      }
+
+      const cursor = taskCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post('/tasks', async(req, res) => {
+      const newTask = req.body;
+      const result = await taskCollection.insertOne(newTask);
       res.send(result)
     })
 
-    app.delete('/allJobs/:id', async(req, res) => {
+    app.delete('/tasks/:id', async(req, res) => {
       const id = req.params.id;
-      const query = { _id: new ObjectId(id)}
-      const result = await jobCollection.deleteOne(query)
-      res.send(result);
+      const query = { _id: new ObjectId(id)};
+      const result = await taskCollection.deleteOne(query)
+      res.send(result)
     })
-
-    // Task manager
-
-
-
-
-
 
     await client.db("admin").command({ ping: 1 });
     console.log(
